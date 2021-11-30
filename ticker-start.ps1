1820a83973c4369b485ba5fc6cb7ae7b9a9a1f9d
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-11-30 04:22:51 -0600

    Create ticker-start.ps1
    add for running w/ WSL

---
+++ ticker-start.ps1
@@ -0,0 +1,8 @@
+#! /usr/bin/pwsh
+
+
+while (1 -eq 1 ){
+    C:\Arch\Arch.exe run './ticker.sh' $Args[0] $Args[1] $Args[2] $Args[3] $Args[4] $Args[5] $Args[6] $Args[7] $Args[8] $Args[9]
+    start-sleep -seconds 10
+    clear
+    }
